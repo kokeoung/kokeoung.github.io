44866c6470725d71080bde77bbc56badf23d9c52
kokeoung <ko908599@gmail.com>
2025-08-25 02:00:19 +0900

    javascript challange complete!

--- js/background.js
+++ js/background.js
@@ -1,8 +1,25 @@
 const images = [
-    "SpiderLogo.jpeg",
-    "SpidermanDark.jpeg",
-    "Spidermanman.jpeg",
-    "Spidermans.jpeg"
+    "astronaunt.jpg",
+    "bluegojyo.jpg",
+    "bluesky.jpg",
+    "castle.jpg",
+    "castlebig.jpg",
+    "castlebig.jpg",
+    "cat.jpg",
+    "fatcat.jpg",
+    "giant.jpg",
+    "giyu.jpg",
+    "giyueye.jpg",
+    "gojyo.jpg",
+    "gozyo.jpg",
+    "moon.jpg",
+    "moonbk.jpg",
+    "mountine.jpg",
+    "sky.jpg",
+    "snoopy.jpg",
+    "space.jpg",
+    "spiderman.jpg",
+    "yellowmoon.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
